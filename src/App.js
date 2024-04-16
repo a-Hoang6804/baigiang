b7e511db8bfccaa21e2cb9a934afc9bc3dd99948
Huynh Anh Hoang <huynhhoang221204@gmail.com>
2024-04-16 15:13:38 +0700

    baigiang

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const App = () => {
+
+  return (
+    <div className="app-container">hello word
+    
+    </div>
+  );
+}
+
+export default App;
