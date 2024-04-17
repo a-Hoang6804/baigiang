da73d782f1d214c7e11c1802eef18d82189077ee
Huynh Anh Hoang <huynhhoang221204@gmail.com>
2024-04-17 16:18:54 +0700

    baidoimau

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import './App.scss'
 import Header from './component/Header/Header';
-const App = () => {
+import { Outlet, Link } from 'react-router-dom';
 
+const App = () => {
   return (
     <div className="app-container">
-    <Header />
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-container'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
