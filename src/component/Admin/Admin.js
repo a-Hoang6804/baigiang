4783b21e5eb237e9d8fb622fe2ffcd3b9216bcbe
Huynh Anh Hoang <huynhhoang221204@gmail.com>
2024-05-06 09:25:24 +0700

    doan13

--- src/component/Admin/Admin.js
+++ src/component/Admin/Admin.js
@@ -3,7 +3,7 @@ import './Admin.scss'
 import { FaBars } from 'react-icons/fa'
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
-
+import  PerfectScrollbar  from 'react-perfect-scrollbar'
 
 
 const Admin = (props) => {
@@ -19,7 +19,9 @@ const Admin = (props) => {
 
                 </div>
                 <div className="admin-main">
+                <PerfectScrollbar>
                     <Outlet />
+                </PerfectScrollbar>
                 </div>
             </div>
 
