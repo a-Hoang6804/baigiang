ec0f5e61a4254aa9f531d8cd92e68fd7582dffbe
Huynh Anh Hoang <huynhhoang221204@gmail.com>
2024-05-17 15:37:34 +0700

    doan26

--- src/component/Admin/Admin.js
+++ src/component/Admin/Admin.js
@@ -3,8 +3,9 @@ import './Admin.scss'
 import { FaBars } from 'react-icons/fa'
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
-import  PerfectScrollbar  from 'react-perfect-scrollbar'
-
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import Langue from '../Header/Langue'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
@@ -15,13 +16,22 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <div className="admin-header">
-                    <FaBars onClick={() => { setCollapsed(!collapsed) }} />
+                    <span onClick={() => { setCollapsed(!collapsed) }}>
+                        <FaBars className="leftside"/>
+                    </span>
+                    <div className="rightside">
+                    <Langue />
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log out</NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
 
                 </div>
                 <div className="admin-main">
-                <PerfectScrollbar>
-                    <Outlet />
-                </PerfectScrollbar>
+                    <PerfectScrollbar>
+                        <Outlet />
+                    </PerfectScrollbar>
                 </div>
             </div>
 
--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -37,7 +37,7 @@ const DetailQuiz = (props) => {
                         answers.push(item.answers);
 
                     })
-
+                    answers=_.orderBy(answers,['id'],['asc']);
 
                     return { questionId: key, answers, questionDescription, image }
                 }
